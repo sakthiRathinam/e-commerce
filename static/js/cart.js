d554f72d98fae7fa6f03fb1f3c2e0362104b375d
sakthiratnam <sakthiratnam050@gmail.com>
2020-05-10 16:43:40 +0530

    added ui and database configuration

---
+++ static/js/cart.js
@@ -0,0 +1,36 @@
+var updateBtns=document.getElementsByClassName('update-cart')
+
+for(let i=0;i<updateBtns.length;i++){
+	updateBtns[i].addEventListener('click',function(){
+		let productId=this.dataset.product;
+		let action=this.dataset.action;
+		console.log("productId:",productId,"action:",action)
+		console.log('user:',user)
+		if(user==='AnonymousUser'){
+			console.log('not logged in')
+		}else{
+			console.log('logged in')
+		}
+	});
+};
+
+
+function updateOrder(productId,action){
+	console.log('User is authenticated, sending data...')
+		var url = '/update_item/'
+		fetch(url, {
+			method:'POST',
+			headers:{
+				'Content-Type':'application/json',
+				'X-CSRFToken':csrftoken,
+			}, 
+			body:JSON.stringify({'productId':productId, 'action':action})
+		})
+		.then((response) => {
+		   return response.json();
+		})
+		.then((data) => {
+		    location.reload()
+		});
+}
+}
